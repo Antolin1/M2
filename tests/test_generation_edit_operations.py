9ac9f6135622dfac39b189185ab783a2d5e366a1
Antolin1 <joseantonio.hernandez6@um.es>
2022-05-24 17:57:17 +0200

    added new example of yakindu model and plot the test

--- tests/test_generation_edit_operations.py
+++ tests/test_generation_edit_operations.py
@@ -1,4 +1,6 @@
 import unittest
+import matplotlib.pyplot as plt
+import networkx as nx
 
 from m2_generator.edit_operation.edit_operation_generation import get_edit_operations
 
@@ -8,8 +10,13 @@ class TestGeneration(unittest.TestCase):
         path_metamodel = "data/yakindu_simplified.ecore"
         all_patterns = get_edit_operations(path_metamodel)
         for patterns in all_patterns:
-            print(len(patterns))
-        self.assertEqual(True, True)  # add assertion here
+            for pattern in patterns:
+                pattern = nx.DiGraph(pattern)
+                plt.figure()
+                pos = nx.spring_layout(pattern)
+                nx.draw(pattern, pos, labels=nx.get_node_attributes(pattern, 'type'), with_labels=True)
+                nx.draw_networkx_edge_labels(pattern, pos, edge_labels=nx.get_edge_attributes(pattern, 'type'))
+                plt.show()
 
 
 if __name__ == '__main__':
